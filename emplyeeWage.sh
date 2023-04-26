3a86f87b4fafda56942fe9d6c44bf6eac27255ca
nilamkurhade <nilamkurhade87@gmail.com>
2023-04-26 09:23:08 +0530

    Added: calculating salary of employee

--- emplyeeWage.sh
+++ emplyeeWage.sh
@@ -10,3 +10,20 @@ then
 else
     echo "Employee is Absent"
 fi
+
+# constant variables
+is_present=1
+full_day_hour=8
+wage_per_hour=20
+
+# initializing the variables
+salary=0
+
+# To check the attendence of the employee
+if [ $attendence_chk -eq $is_present ]
+then
+    salary=$(($full_day_hour * $wage_per_hour))
+    echo "The Employee is Present for Full day and the Salary is $salary"
+else
+    echo "Employee is Absent and the salary is $salary"
+fi
