dc9b6d28fd87d11f5f4ad9d294b0ef2a2c5d88e6
nilamkurhade <nilamkurhade87@gmail.com>
2023-04-27 08:38:52 +0530

    Added: calculating wage for part time

--- emplyeeWage.sh
+++ emplyeeWage.sh
@@ -27,3 +27,28 @@ then
 else
     echo "Employee is Absent and the salary is $salary"
 fi
+
+echo "Calculating Wage for Part Time Employee"
+
+# constant variables
+is_present_full_time=1
+is_present_part_time=2
+part_time_hour=4
+
+# initializing the variables
+salary=0
+
+# To check the attendence of the employee
+if [ $attendence_chk -eq $is_present_full_time ]
+then
+    salary=$(($full_day_hour * $wage_per_hour))
+    echo "The Employee is Present for Full day and the Salary is $salary"
+
+elif [ $attendence_chk -eq $is_present_part_time ]
+then
+    salary=$(($part_time_hour * $wage_per_hour))
+    echo "The Employee is Present for Half day and the Salary is $salary"
+
+else
+    echo "The Employee is Absent and the salary is $salary"
+fi
